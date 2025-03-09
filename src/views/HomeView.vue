7831eb0e23c2f75add6630fbcec5148a793e22a4
Jamal Fox <jamalfox@Mac.attlocal.net>
2025-03-09 13:28:59 -0400

    set up vue router

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
+  <main class="main_wrapper">
+    <h1>Test</h1>
   </main>
 </template>
+<script lang="ts">
+export default {};
+</script>
---
+++ src/views/PlanListView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="about">
+    <h1>Plan list view</h1>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style></style>
